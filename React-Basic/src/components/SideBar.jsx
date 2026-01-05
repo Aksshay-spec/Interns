d26c1ed8709614c4b1062d203e3dbd2c852be84d
Savra Akshay <savaraakshay2366@gmail.com>
2026-01-05 11:24:30 +0530

    modif sidebar

--- React-Basic/src/components/SideBar.jsx
+++ React-Basic/src/components/SideBar.jsx
@@ -5,10 +5,10 @@ const Sidebar = ({data}) => {
  
   console.log(data);
   return (
-    <div className="sidebar">
+    <div className="sidebar"> 
       <ul>
         {data ? data.data.map((item) => (
-          <li key={item.id}>name : {item.name} email : {item.age}</li>
+          <li key={item.id}>name : {item.name} Age : {item.age}</li>
         )) : <li>Loading...</li>}
       </ul>
     </div>
