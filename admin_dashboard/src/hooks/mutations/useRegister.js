a07fef9671e39af4ed35679a19ec520e6a76af8c
akshaysavra <savaraakshay2366@gmail.com>
2026-03-10 09:28:42 +0530

    Delete admin_dashboard directory

--- admin_dashboard/src/hooks/mutations/useRegister.js
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import api  from "../../apis/axios";
-
- const registerUser = (data) =>
-  api.post("/register", data);
-
-export const useRegister = () =>
-  useMutation({ mutationFn: registerUser });
